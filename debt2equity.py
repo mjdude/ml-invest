f49d210ed18b88a7471bd4063fe0ce684804a863
mjdude <mo@syncspot.net>
2017-07-07 19:37:08 -0700

    add ticker to data frame and save to csv

--- debt2equity.py
+++ debt2equity.py
@@ -10,24 +10,31 @@ def Key_Stats(gather='Total Debt/Equity (mrq)'):
     statspath = path + '/_KeyStats'
     stock_list = [x[0] for x in os.walk(statspath)]
     #print(stock_list)
-
+    df = pd.DataFrame(columns = ['Date', 'Unix', 'Ticker', 'DE Ratio'])
     for each_dir in stock_list[1:]:
         each_file = os.listdir(each_dir)
         ticker = each_dir.split("KeyStats/")[1]
         #make sure to skip folders with no files in them
         if len(each_file) > 0:
             for file in each_file:
-                data_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
-                unix_time = time.mktime(data_stamp.timetuple())
-                #print(data_stamp, unix_time)
+                date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
+                unix_time = time.mktime(date_stamp.timetuple())
+                # print(date_stamp, unix_time)
                 full_file_path = each_dir + '/' + file
                 #print(full_file_path)
                 source = open(full_file_path, 'r').read()
                 #print(source)
-                value = source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
-
-                print(ticker + ":" , value)
-                time.sleep(15)
+                try:
+                    value = float(source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+                    df = df.append({'Date' : date_stamp, 'Unix': unix_time , 'Ticker' : ticker , 'DE Ratio' :value ,}, ignore_index = True)
+                except Exception as e:
+                    pass
+                
+                save = gather.replace(' ', '').replace(')', '').replace('(', '').replace('/','')+('.csv')
+                print(save)
+                df.to_csv(save)
+                #print(ticker + ":" , value)
+              #  time.sleep(15)
 
 
 Key_Stats()

96097582af3d3467a42658835f9fc4d981c82c39
mjdude <mo@syncspot.net>
2017-07-07 19:18:13 -0700

    extract ticker and value

--- debt2equity.py
+++ debt2equity.py
@@ -13,12 +13,20 @@ def Key_Stats(gather='Total Debt/Equity (mrq)'):
 
     for each_dir in stock_list[1:]:
         each_file = os.listdir(each_dir)
+        ticker = each_dir.split("KeyStats/")[1]
         #make sure to skip folders with no files in them
         if len(each_file) > 0:
             for file in each_file:
                 data_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
                 unix_time = time.mktime(data_stamp.timetuple())
-                print(data_stamp, unix_time)
+                #print(data_stamp, unix_time)
+                full_file_path = each_dir + '/' + file
+                #print(full_file_path)
+                source = open(full_file_path, 'r').read()
+                #print(source)
+                value = source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
+
+                print(ticker + ":" , value)
                 time.sleep(15)
 
 

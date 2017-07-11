9110e635e67d6c51ec7db7a6fbffacdbab5d5e3d
mjdude <mo@syncspot.net>
2017-07-11 14:44:01 -0700

    fixed bug (added dataset from 2008)

--- debt2equity.py
+++ debt2equity.py
@@ -12,7 +12,7 @@ def Key_Stats(gather='Total Debt/Equity (mrq)'):
     df = pd.DataFrame(columns = ['Date', 'Unix', 'Ticker', 'DE Ratio'])
     sp500_df = pd.DataFrame.from_csv('data/yahoo-s&p/^GSPC.csv')
 
-    for each_dir in stock_list[1:10]:
+    for each_dir in stock_list[1:]:
         # print('dir is ', each_dir )
         each_file = os.listdir(each_dir)
         ticker = each_dir.split("KeyStats/")[1]
@@ -30,15 +30,17 @@ def Key_Stats(gather='Total Debt/Equity (mrq)'):
                     # print('value is ', value)
                     try:
                         sp500_date = datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d')
-                        print('date is ', sp500_date)
+                        # print('date is ', sp500_date)
                         row = sp500_df[(sp500_df.index == sp500_date)]
-                        print('row is :', row)
+                        # print('row is :', row)
                         sp500_value = float(row['Adj Close'])
                     except Exception as e:
-                        print(e)
+                        # print(e)
                         # If on the weekend we go back 3 days (259200) so we get data from the weekday
                         sp500_date = datetime.fromtimestamp(unix_time-259200).strftime('%Y-%m-%d')
+                        # print('date is ', sp500_date)
                         row = sp500_df[(sp500_df.index == sp500_date)]
+                        # print('row is :', row)
                         sp500_value = float(row['Adj Close'])
                     
                     stock_price = float(source.split('</small><big><b>')[1].split('</b></big>')[0])
